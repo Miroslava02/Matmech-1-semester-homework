6aa48421b2e2d2732903aafed1a97d8bf1c067ca
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-11-04 16:25:38 +0300

    Add files via upload

---
+++ 22.10.2020(1).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector <int> A, int k) {
+    vector <int> Left;
+    vector <int> Right;
+    for (int i = 1; i < A.size(); i++) {
+        if (A[i] < A[0]) {
+            Left.push_back(A[i]);
+        }
+        else {
+            Right.push_back(A[i]);
+        }
+    }
+    if (Left.size() + 1 == k) return(A[0]);
+    if (Left.size() + 1 < k) return search(Right, k - Left.size() - 1);
+    if (Left.size() + 1 > k) return search(Left, k);
+}
+
+int main()
+{
+    vector <int> Array;
+    int k, n, x;
+    cout << "enter array length";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        Array.push_back(x);
+    }
+    cout << "enter k";
+    cin >> k;
+    cout << search(Array, k);
+    system("PAUSE");
+    return 0;
+}
+
