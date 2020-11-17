f9c80a37651d2cde1cc9d35f23d3d1e24e92758c
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-11-17 20:03:35 +0300

    Add files via upload

---
+++ 12.11.2020(1).cpp
@@ -0,0 +1,57 @@
+﻿#include "math.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int search(vector <int> A, int k) {
+	vector <int> Left;
+	vector <int> Right;
+	for (int i = 1; i < A.size(); i++) {
+		if (A[i] < A[0]) {
+			Left.push_back(A[i]);
+		}
+		else {
+			Right.push_back(A[i]);
+		}
+	}
+	if (A[Left.size()] == k) return(A[0]);
+	if ((A[0] < k) && (Right.size() != 0))return search(Right, k);
+	if ((A[0] < k) && (Right.size() == 0))return 0;
+	if ((A[0] > k) && (Left.size() != 0)) return search(Left, k);
+	if ((A[0] > k) && (Left.size() == 0)) return 0;
+}
+
+int main() {
+	int k;
+	int n;
+	vector <int> prime;
+	cin >> k;
+	int* composite = new int[k];
+	cin >> n;
+	int* Array = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> Array[i];
+	}
+	for (int i = 0; i < k; i++) {
+		composite[i] = i;
+	}
+	for (int p = 2; p < (sqrt(k)); p++) {
+		if (composite[p] != 0) {
+			for (int i = p * p; i < k; i += p) {
+				composite[i] = 0;
+			}
+		}
+	}
+	for (int i = 2; i < k; i++) {
+		if (composite[i] != 0) {
+			prime.push_back(composite[i]);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (search(prime, Array[i]) != 0) {
+			cout << Array[i] << " ";
+		}
+	}
+	system("PAUSE");
+	return 0;
+}
---
+++ 12.11.2020(2).cpp
@@ -0,0 +1,36 @@
+﻿//ищем количество подпоследовательностей в массиве длины n, cумма элементов которых не превышает K
+//
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	 int i = 0;
+	 int j = 1;
+	 int S = 0;
+	 int R = 0;
+	 int n;
+	 cin >> n;
+	 int K;
+	 cin >> K;
+	 int* A = new int [n];
+	 for (int i = 0; i < n; i++) {
+		 cin >> A[i];
+	 }
+	 while (i < n - 1) {
+		 i++;
+		 S += A[i];
+		 while ((S > K) && (j < i)) {
+			 S -= A[j];
+			 j++;
+		 }
+		 if (j <= i) {
+			 R += i + 1 - j;
+		 }
+		 else S += 0;
+	 }
+	 cout << R;
+	 system("PAUSE");
+	 return 0;
+}
+
