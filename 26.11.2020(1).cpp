39adbbab44ae391f7ddec5e618957bee354fa162
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-08 21:44:58 +0300

    Add files via upload

---
+++ 26.11.2020(1).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+int Min(const unsigned int m, const unsigned int n, unsigned int A[m][n]) {
+    if ((m != 0) && (n != 0)) {
+        return min(Min(&A, m - 1, n), Min(&A, m, n - 1), Min(&A, m - 1, n - 1)) + A[m][n];
+    }
+
+    if ((m != 0) && (n == 0)) {
+        return A[m][0] + Min(&A, m - 1, n);
+    }
+
+    if ((m == 0) && (n != 0)) {
+        return A[0][n] + Min(&A, m, n - 1);
+    }
+    if ((m == 0) && (n == 0)) {
+        return A[0][0];
+    }
+
+}
+
+int main() {
+    unsigned int x, y;
+    cin >> x >> y;
+    const unsigned int m = x;
+    const unsigned int n = y;
+    unsigned int Matrix[m][n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            Matrix[i][j] = rand();
+        }
+    }
+    cout << Min(&Matrix, m, n);
+    system("PAUSE");
+    return 0;
+}
---
+++ 26.11.2020(2).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string A, B;
+    int S, j, m;
+    j = 0;
+    S = 0;
+    cin >> A >> B;
+    for (int i = 0; i < A.size(); i++) {
+        if (j < B.size()) {
+            if (A[i] != A[j]) {
+                S++;
+            }
+            else {
+                j++;
+                m = i;
+            }
+        }
+    }
+    S += max(A.size() - m, B.size() - j);
+    cout << S;
+    system("PAUSE");
+    return 0;
+}
