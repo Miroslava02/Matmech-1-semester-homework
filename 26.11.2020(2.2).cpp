34d48c591f7e608cc03b2cbffd41dbf0bc8ddde4
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-11 20:51:04 +0300

    Update 26.11.2020(2.2).cpp

--- 26.11.2020(2.2).cpp
+++ 26.11.2020(2.2).cpp
@@ -1,23 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 using namespace std;
 
-int compare(string &A, string &B, int a, int b) {
+int compare(string& A, string& B, int a, int b) {
     int s = 0;
     for (int i = 0; i < min(a, b); i++) {
         if (A[i] != B[i]);
         s += 1;
     }
-    retiurn s;
+    return s;
 }
 
-int minimum(string &A, string &B) {
-    int** D = new int*[A.size()]
-    for (int i = 0; i < B.size(); i++) {
-            D[i] = new int[B.size()];
-    }
-    for (int i = 0; i < A.size(); i++) {
-        for (int j = 0; j < B.size(); j++) {
+int minimum(string& A, string& B) {
+    int** D = new int* [A.size() + 1];
+    for (int i = 0; i <= A.size(); i++) {
+            D[i] = new int[B.size() + 1];
+        }
+    for (int i = 0; i <= A.size(); i++) {
+        for (int j = 0; j <= B.size(); j++) {
             if ((i != 0) && (j != 0)) {
                 D[i][j] = min(min(D[i - 1][j] + 1, D[i][j - 1] + 1), D[i - 1][j - 1] + compare(A, B, i, j));
             }
@@ -27,9 +27,9 @@ int minimum(string &A, string &B) {
             else if (i == 0) {
                 D[i][j] = j;
             }
-            
+
         }
-        return D[A.size()][B.size()]
+        return D[A.size()][B.size()];
     }
 }
 
