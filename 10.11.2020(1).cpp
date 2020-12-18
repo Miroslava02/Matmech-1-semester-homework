f8622eab9b111eaffba54fc7a3a4b4193d57dfec
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-18 23:41:41 +0300

    Add files via upload

---
+++ 10.11.2020(1).cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int sum(int n, int k, int* &S, int* &W) {
+	int **D = new int* [k+1];
+	for (int i = 0; i <= k; i++) {
+		D[i] =new int[n + 1];
+	}
+	for (int i = 0; i <=k; i++) {
+		for (int j = 0; j <= n; j++) {
+			if (i == 0 or j == 0) {
+				D[i][j] = 0;
+			}
+			else {
+				if (W[i] < j) {
+					D[i][j] = max(D[i - 1][j - W[i]] + S[i], D[i - 1][j]);
+				}
+				else {
+					D[i][j] = D[i - 1][j];
+				}
+			}
+		}
+	}
+	return D[k][n];
+}
+
+int main() {
+	int n, k;
+	cin >> n;
+	cin >> k;
+	int*Sum = new int[k+1];
+    int*Weight = new int[k+1];
+	Sum[0]=0;
+	Weight[0]=0;
+	for (int i = 1; i <= k; i++) {
+		cout << "enter price";
+		cin >> Sum[i];
+		cout << "enter weight";
+		cin >> Weight[i];
+	}
+	cout << sum(n, k, Sum, Weight);
+	system("PAUSE");
+	return(0);
+}
