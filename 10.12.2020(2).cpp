c74cfa2aedf073843bdd48c9f1891364a5204b54
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-19 19:01:45 +0300

    Add files via upload

---
+++ 10.12.2020(2).cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int compare(string& A, string& B, int i, int j) {
+    if (A[i - 1] == B[j - 1]) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+
+}
+
+int minimum(string& A, string& B, int a, int b, int c) {
+    int** D = new int* [A.length() + 1];
+    for (int i = 0; i <= A.length(); i++) {
+        D[i] = new int[B.length() + 1];
+    }
+    for (int i = 0; i <= A.length(); i++) {
+        for (int j = 0; j <= B.length(); j++) {
+            if ((i != 0) && (j != 0)) {
+                D[i][j] = min(min(D[i - 1][j] + a, D[i][j - 1] + b), D[i - 1][j - 1] + c*compare(A, B, i, j));
+            }
+            else if (j == 0) {
+                D[i][j] = i;
+            }
+            else if (i == 0) {
+                D[i][j] = j;
+            }
+        }
+    }
+    return D[A.size()][B.size()];
+}
+
+int main() {
+    string A, B;
+    int a, b, c;
+    cin >> A;
+    cin >> B;
+    cout << "delete price";
+    cin >> a;
+    cout << "add price";
+    cin >> b;
+    cout << "change price";
+    cin >> c;
+    cout << minimum(A, B, a, b, c);
+    system("PAUSE");
+    return 0;
+}
