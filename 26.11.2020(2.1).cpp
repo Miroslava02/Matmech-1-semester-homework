2f518b0c9b9145512a4de46d037e08bc60d9f81d
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-19 18:40:16 +0300

    Add files via upload

---
+++ 26.11.2020(2.1).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int compare(string& A, string& B, int i, int j) {
+    if (A[i - 1] == B[j- 1]) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+
+}
+
+int minimum(string& A, string& B) {
+    int** D = new int* [A.length() + 1];
+    for (int i = 0; i <= A.length(); i++) {
+        D[i] = new int[B.length() + 1];
+    }
+    for (int i = 0; i <= A.length(); i++) {
+        for (int j = 0; j <= B.length(); j++) {
+            if ((i != 0) && (j != 0)) {
+                D[i][j] = min(min(D[i - 1][j] + 1, D[i][j - 1] + 1), D[i - 1][j - 1] + compare(A, B, i, j));
+            }
+            else if (j == 0) {
+                D[i][j] = i;
+            }
+            else if (i == 0) {
+                D[i][j] = j;
+            } 
+        }
+    }
+    return D[A.size()][B.size()];
+}
+
+int main() {
+    string A, B;
+    cin >> A;
+    cin >> B;
+    cout << minimum(A, B);
+    system("PAUSE");
+    return 0;
+}
