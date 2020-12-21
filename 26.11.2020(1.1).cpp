221da5e939af8060bd1972ab8ad9aabfffc6e214
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-21 19:23:12 +0300

    Update 26.11.2020(1.1).cpp

--- 26.11.2020(1.1).cpp
+++ 26.11.2020(1.1).cpp
@@ -1,29 +1,42 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include "math.h"
 using namespace std;
 
 int S(int N) {
     double a = sqrt(N);
+    cout << a;
     int b = int(a);
-    int** D = new int* [b+1];
+    cout << b;
+    int** D = new int* [b + 1];
     for (int i = 0; i <= sqrt(N); i++) {
         D[i] = new int[N + 1];
-    }
-    for (int i = 0; i <= sqrt(N); i++) {
         for (int j = 0; j <= N; j++) {
-            if ((i == 0) or (j == 0)) {
+            if (i == 0) {
+                D[i][j] = N + 1;
+            }
+            else if (j == 0) {
                 D[i][j] = 0;
             }
             else {
-                D[i][j] = min(D[i - 1][j], (D[i][j - i * i] + 1));
+                if (j >= i * i) {
+                    D[i][j] = min(D[i - 1][j], (D[i][j - i * i] + 1));
+                }
+                else {
+                    D[i][j] = D[i - 1][j];
+                }
             }
+            cout<<D[i][j]<<" ";
+        }
+        if (i != 0) {
+            delete[] D[i - 1];
         }
     }
     return D[b][N];
-}
+    delete[] D[b];
+ }
 
-int main() {
+int main(){
     int N;
     cin >> N;
     cout << S(N);
