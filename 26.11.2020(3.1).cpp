e6791422ae5a6e6c514ee635a96a308fd6f7b10e
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2020-12-21 18:34:31 +0300

    Update 26.11.2020(3.1).cpp

--- 26.11.2020(3.1).cpp
+++ 26.11.2020(3.1).cpp
@@ -1,17 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 #include <vector>
 using namespace std;
 
 int Min(int** &A, int m, int n) {
-    int** D = new int* [m];
+    int** D = new int*[m];
     for (int i = 0; i < m; i++) {
         D[i] = new int[n];
-    }
-    for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if ((i != 0) && (j != 0)) {
-                D[i][j] = min(min(D[i-1][n], D[i][j - 1]), min(D[i-1][j-1], D[i][j-1])) + A[i][j];
+                D[i][j] = min(min(D[i - 1][j], D[i][j - 1]), D[i - 1][j - 1]) + A[i][j];
             }
             else if ((i != 0) && (j == 0)) {
                 D[i][j] = D[i - 1][j] + A[i][j];
@@ -23,8 +21,12 @@ int Min(int** &A, int m, int n) {
                 D[i][j] = A[i][j];
             }
         }
+        if (i != 0) {
+            delete[] D[i - 1];
+        }
     }
-    return D[m-1][n-1];
+    return D[m - 1][n - 1];
+    delete[] D[m - 1];
 }
 
 int main() {
@@ -33,7 +35,7 @@ int main() {
     cin >> n;
     int** Matrix = new int* [m];
     for (int i = 0; i < m; i++) {
-        Matrix[i] = new int [n];
+        Matrix[i] = new int[n];
     }
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -41,6 +43,9 @@ int main() {
         }
     }
     cout << Min(Matrix, m, n);
+    for (int i = 0; i < m; i++) {
+        delete[] Matrix[i];
+    }
     system("PAUSE");
     return 0;
 }
